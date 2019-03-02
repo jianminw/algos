3ec9ca2c50cd84f545362dfb8a0613df650e21e3
Jianming <jianminw@andrew.cmu.edu>
2019-03-02 13:15:25 -0500

    removed starter methods

--- katrina_algo/algo_strategy.py
+++ katrina_algo/algo_strategy.py
@@ -58,6 +58,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
         #game_state.suppress_warnings(True)  #Uncomment this line to suppress warnings.
 
+        self.get_diagnostics(game_state)
         self.starter_strategy(game_state)
 
         game_state.submit_turn()
@@ -105,100 +106,6 @@ class AlgoStrategy(gamelib.AlgoCore):
             while( game_state.number_affordable(EMP) > 1):
                 game_state.attempt_spawn(EMP, [3, 10])
         
-
-    def deploy_attackers(self, game_state):
-        """
-        First lets check if we have 10 bits, if we don't we lets wait for
-        a turn where we do.
-        """
-        if (game_state.get_resource(game_state.BITS) < 10):
-            return
-
-        """
-        First lets deploy an EMP long range unit to destroy firewalls for us.
-        """
-        if game_state.can_spawn(EMP, [3, 10]):
-            game_state.attempt_spawn(EMP, [3, 10])
-
-        """
-        Now lets send out 3 Pings to hopefully score, we can spawn multiple
-        information units in the same location.
-        """
-        if game_state.can_spawn(PING, [14, 0], 3):
-            game_state.attempt_spawn(PING, [14,0], 3)
-
-        """
-        NOTE: the locations we used above to spawn information units may become
-        blocked by our own firewalls. We'll leave it to you to fix that issue
-        yourselves.
-
-        Lastly lets send out Scramblers to help destroy enemy information units.
-        A complex algo would predict where the enemy is going to send units and
-        develop its strategy around that. But this algo is simple so lets just
-        send out scramblers in random locations and hope for the best.
-
-        Firstly information units can only deploy on our edges. So lets get a
-        list of those locations.
-        """
-        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-
-        """
-        Remove locations that are blocked by our own firewalls since we can't
-        deploy units there.
-        """
-        deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-
-        """
-        While we have remaining bits to spend lets send out scramblers randomly.
-        """
-        while game_state.get_resource(game_state.BITS) >= game_state.type_cost(SCRAMBLER) and len(deploy_locations) > 0:
-
-            """
-            Choose a random deploy location.
-            """
-            deploy_index = random.randint(0, len(deploy_locations) - 1)
-            deploy_location = deploy_locations[deploy_index]
-
-            game_state.attempt_spawn(SCRAMBLER, deploy_location)
-            """
-            We don't have to remove the location since multiple information
-            units can occupy the same space.
-            """
-
-    def filter_blocked_locations(self, locations, game_state):
-        filtered = []
-        for location in locations:
-            if not game_state.contains_stationary_unit(location):
-                filtered.append(location)
-        return filtered
-
-    def build_c1_logo(self, game_state):
-        """
-        We use Filter firewalls because they are cheap
-
-        First, we build the letter C.
-        """
-        firewall_locations = [[8, 11], [9, 11], [7,10], [7, 9], [7, 8], [8, 7], [9, 7]]
-        for location in firewall_locations:
-            if game_state.can_spawn(FILTER, location):
-                game_state.attempt_spawn(FILTER, location)
-
-        """
-        Build the number 1.
-        """
-        firewall_locations = [[17, 11], [18, 11], [18, 10], [18, 9], [18, 8], [17, 7], [18, 7], [19,7]]
-        for location in firewall_locations:
-            if game_state.can_spawn(FILTER, location):
-                game_state.attempt_spawn(FILTER, location)
-
-        """
-        Build 3 dots with destructors so it looks neat.
-        """
-        firewall_locations = [[11, 7], [13, 9], [15, 11]]
-        for location in firewall_locations:
-            if game_state.can_spawn(DESTRUCTOR, location):
-                game_state.attempt_spawn(DESTRUCTOR, location)
-
 
     ####### Begin Bryce's functions ########
 
@@ -250,7 +157,7 @@ class AlgoStrategy(gamelib.AlgoCore):
             elif(i % 2 == 0 and game_state.can_spawn(ENCRYPTOR, (i, 9))):
                 locs.append[(i, 9)]
             elif(game_state.can_spawn(ENCRYPTOR, (25 - i//2, 9))):
-                locs.append[(25 - i//2, 9))]
+                locs.append[(25 - i//2, 9)]
 
         return locs
 
